a41034cf950a7ec20f5cf077de6974055d1f1ab8
HarrisonDebz <harrisonmalisawa16@gmail.com>
2026-02-09 23:48:18 +0200

    Fixed landing page logic

--- catv1/Platforms/Android/MainApplication.cs
+++ catv1/Platforms/Android/MainApplication.cs
@@ -6,10 +6,21 @@ namespace catv1;
 [Application]
 public class MainApplication : MauiApplication
 {
-	public MainApplication(IntPtr handle, JniHandleOwnership ownership)
-		: base(handle, ownership)
-	{
-	}
+    public MainApplication(IntPtr handle, JniHandleOwnership ownership)
+        : base(handle, ownership)
+    {
+    }
 
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    protected override MauiApp CreateMauiApp()
+    {
+        try
+        {
+            return MauiProgram.CreateMauiApp();
+        }
+        catch (Exception ex)
+        {
+            Android.Util.Log.Error("CAT_CRASH_ERROR", $"Fatal exception during CreateMauiApp: {ex}");
+            throw;
+        }
+    }
 }
