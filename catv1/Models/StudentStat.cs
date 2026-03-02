9b357b24713fd668a7f16c909f60a3838b7fa1a2
HarrisonDebz <harrisonmalisawa16@gmail.com>
2026-03-02 21:42:13 +0200

    Changes

--- catv1/Models/StudentStat.cs
+++ catv1/Models/StudentStat.cs
@@ -4,7 +4,7 @@ public class StudentStat
 {
     public string? Name { get; set; }
     public string? Id { get; set; }
-    public string? Avatar { get; set; } // URL or resource name
+    public string? Avatar { get; set; } // URL
     public double PresenceRate { get; set; }
     public int Absences { get; set; }
     public string PresenceRateDisplay => $"{PresenceRate:P0} Rate";
