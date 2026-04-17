718ffa1b8472e3068d1f6e475727bdcb370da87a
HarrisonDebz <harrisonmalisawa16@gmail.com>
2026-04-17 20:03:41 +0200

    feat: implement MVVM architecture with base and feature-specific ViewModels for lecturer and student interfaces

--- catv1/ViewModels/BaseViewModel.cs
+++ catv1/ViewModels/BaseViewModel.cs
@@ -1,9 +1,9 @@
-using System.ComponentModel;
+using CommunityToolkit.Mvvm.ComponentModel;
 using System.Runtime.CompilerServices;
 
 namespace catv1.ViewModels;
 
-public class BaseViewModel : INotifyPropertyChanged
+public partial class BaseViewModel : ObservableObject
 {
     private bool _isBusy;
     public bool IsBusy
@@ -25,23 +25,6 @@ public class BaseViewModel : INotifyPropertyChanged
     {
         get => _title;
         set => SetProperty(ref _title, value);
-    }
-
-    public event PropertyChangedEventHandler? PropertyChanged;
-
-    protected void OnPropertyChanged([CallerMemberName] string? propertyName = null)
-    {
-        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-    }
-
-    protected bool SetProperty<T>(ref T backingStore, T value, [CallerMemberName] string propertyName = "")
-    {
-        if (EqualityComparer<T>.Default.Equals(backingStore, value))
-            return false;
-
-        backingStore = value;
-        OnPropertyChanged(propertyName);
-        return true;
     }
 
     public string CurrentDateDisplay => DateTime.Now.ToString("dddd, MMMM dd, yyyy");
