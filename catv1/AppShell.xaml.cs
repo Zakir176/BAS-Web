19a0bac9045744dcee65cca2a9303e5c2f5ddea8
Harrison Malisawa <152499260+HarrisonDebz@users.noreply.github.com>
2026-06-19 21:55:20 +0200

    Use specific pages in AppShell and add routes
    Replace generic HistoryPage entries with StudentHistoryPage and LecturerReportsPage in AppShell.xaml, and register three new routes (SettingsPage, CourseListPage, NotificationsPage) in AppShell.xaml.cs so these views can be navigated via Shell routing.
    
    Co-Authored-By: Zakir Motala  <zakirhusseinmotala76@gmail.com>

--- catv1/AppShell.xaml.cs
+++ catv1/AppShell.xaml.cs
@@ -8,6 +8,9 @@ public partial class AppShell : Shell
         InitializeComponent();
         Routing.RegisterRoute("signup", typeof(Views.SignUpPage));
         Routing.RegisterRoute("barcode", typeof(Views.BarcodePage));
+        Routing.RegisterRoute("settings", typeof(Views.SettingsPage));
+        Routing.RegisterRoute("courseList", typeof(Views.CourseListPage));
+        Routing.RegisterRoute("notifications", typeof(Views.NotificationsPage));
         System.Diagnostics.Debug.WriteLine("CAT_LOG: AppShell Constructor End");
     }
 }
